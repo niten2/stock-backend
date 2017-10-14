767591cd4acbf741665e4894d7fb03431114d786
Niten <niten2@yandex.ru>
2017-10-14 21:10:57 +0700

    edit routers

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
   "os"
 	"net/http"
   "github.com/joho/godotenv"
+
+  "go-todo-api/db"
 	"go-todo-api/routers"
 )
 
+func init() {
+  _ = godotenv.Load()
+	db.Connect()
+}
+
 func main() {
   _ = godotenv.Load()
-	http.ListenAndServe(os.Getenv("PORT"), routers.Handlers())
   fmt.Println("server run")
+	http.ListenAndServe(os.Getenv("PORT"), routers.Handlers())
 }
--- routers/routers.go
+++ routers/routers.go
@@ -1,17 +1,38 @@
 package routers
 
 import (
+  "fmt"
   "net/http"
   "encoding/json"
   "github.com/gorilla/mux"
+  "github.com/rs/cors"
+  "io/ioutil"
+  // "bytes"
   "go-todo-api/graphql"
 )
 
-func Handlers() *mux.Router {
+// type test_struct struct {
+
+
+// }
+
+
+func Handlers() http.Handler {
   r := mux.NewRouter()
 	r.HandleFunc("/", mainHandler)
 	r.HandleFunc("/v1", graphqlHandler)
-  return r
+
+  c := cors.New(cors.Options{
+    AllowedOrigins: []string{"*"},
+    AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"},
+    AllowedHeaders: []string{"*"},
+    AllowCredentials: true,
+    // Debug: true,
+  })
+
+  handler := c.Handler(r)
+
+  return handler
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +41,15 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func graphqlHandler(w http.ResponseWriter, r *http.Request) {
-  result := graphql.ExecuteQuery(r.URL.Query().Get("query"))
+  // query := r.URL.Query().Get("query")
+
+  query, _ := ioutil.ReadAll(r.Body)
+  var dat map[string]string
+
+  if err := json.Unmarshal(query, &dat); err != nil { panic(err) }
+
+  fmt.Println(dat["query"])
+
+  result := graphql.ExecuteQuery(dat["query"])
   json.NewEncoder(w).Encode(result)
 }
